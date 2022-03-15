4387d636bfc763092e014e3edc9908c63ea17e78
Christian <43299063+Christiandecolombia@users.noreply.github.com>
2022-03-15 13:11:24 -0700

    working on game controller

--- src/main/java/com/wordgame/controllers/GameController.java
+++ src/main/java/com/wordgame/controllers/GameController.java
@@ -9,14 +9,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
+
 public class GameController {
     // receive words from up to 4 users
 
-    @MessageMapping("/game")
-    @SendTo("/topic/greetings")
-    public PlayerGuess guess(PlayerGuess guess) throws Exception {
-        Thread.sleep(1000); // simulated delay
-        return new PlayerGuess("Player Guess, " + HtmlUtils.htmlEscape(guess.getGuess()) + "!");
-    }
+//    @MessageMapping("/game")
+//    @SendTo("/game/greetings")
+//    public PlayerGuess guess(PlayerGuess guess) throws Exception {
+//        Thread.sleep(1000); // simulated delay
+//        return new PlayerGuess("Player Guess, " + HtmlUtils.htmlEscape(guess.getGuess()) + "!");
+//    }
 
+    // Used to send a one time message to users, such as subscribing to a game
+//    @Controller
+//    public class SubscribeMappingController {
+//        @SubscribeMapping("/subscribe")
+//        public String sendOneTimeMessage() {
+//            return "server one-time message via the application";
+//        }
+//    }
+
+    // Used for repetitive messaging from application to clients.
+//    @Controller
+//    public class MessageMappingController {
+//        @MessageMapping("/request")
+//        public void handleMessageWithoutResponse(String message) {
+//            logger.info("Message without response: {}", message);
+//        }
+//    }
 }
