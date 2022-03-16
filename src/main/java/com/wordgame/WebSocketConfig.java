7f6814457f73be91d275acb82a5c0f1d6af9970a
Christian <43299063+Christiandecolombia@users.noreply.github.com>
2022-03-16 15:36:34 -0700

    Revert "Revert "Christian feature game controller""
    This reverts commit b05ab8a451e4ef2935dbb750bc87d0b6dfd4ed64.

--- src/main/java/com/wordgame/WebSocketConfig.java
+++ src/main/java/com/wordgame/WebSocketConfig.java
@@ -15,12 +15,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/player");
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/connect").withSockJS();
     }
 
     @Override
