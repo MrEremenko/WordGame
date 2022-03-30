283ead323bfd286e5593ea8146c8075cbfc998d3
Christian <43299063+Christiandecolombia@users.noreply.github.com>
2022-03-30 13:03:34 -0700

    moved old code to game.js

--- src/main/frontend/components/NavBar.js
+++ src/main/frontend/components/NavBar.js
@@ -19,37 +19,36 @@ const NavBar = props => {
     }, [])
 
     return (
-    <div className="">
+    <div className="container">
         <div className="header">
             <h1> Word Game </h1>
-         </div>
-      <div>
-        <Router>
-            <ul>
-              <li>
-                <Link to="/home">Home</Link>
-              </li>
-              <li>
-                <Link to="/game">Game</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/test3">Test 3</Link>
-              </li>
-            </ul>
-            <hr />
-               <Switch>
-                 <Route exact path="/">
-                   <Redirect to="/home"/>
-                 </Route>
-                 <Route exact path="/home" component={Home}/>
-                 <Route exact path="/game" component={Game}/>
-                 <Route exact path="/about" component={AboutUs}/>
-               </Switch>
-       </Router>
-     </div>
+        </div>
+        <div>
+            <Router>
+                <div className="NavBar">
+                    <ul>
+                        <li>
+                            <Link to="/home">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/game">Game</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About Us</Link>
+                        </li>
+                    </ul>
+                <hr />
+                </div>
+                <Switch>
+                    <Route exact path="/">
+                        <Redirect to="/home"/>
+                    </Route>
+                    <Route exact path="/home" component={Home}/>
+                    <Route exact path="/game" component={Game}/>
+                    <Route exact path="/about" component={AboutUs}/>
+                </Switch>
+            </Router>
+        </div>
     </div>
     )
 }
